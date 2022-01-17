4095a5bdb5fe749459b41d1601a7ccc3b7aa1de3
joechip125 <52743358+joechip125@users.noreply.github.com>
2022-01-17 14:12:15 +0100

    Add turboCollections project

---
+++ TurboCollections/TurboList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace TurboCollections
+{
+    public class TurboList<T>
+    {
+        private T [] _items = Array.Empty<T>();
+        private int _size = 0;
+        
+        TurboList()
+        {
+            
+        }
+
+        public int Count()
+        {
+            return _size;
+        }
+
+        public void Add(T item)
+        {
+            T[] array = _items;
+            int size = _size;
+
+            if (size < array.Length)
+            {
+                _size = size + 1;
+                array[size] = item;
+            }
+            else
+            {
+                
+            }
+        }
+
+        public T Get(int index)
+        {
+            if (_items.Length <= index)
+            {
+                return _items[index];
+            }
+            else
+            {
+                throw new System.Exception("index outside range of List");    
+            }
+        }
+
+        void RemoveAt(int index)
+        {
+            
+        }
+    }
+}
